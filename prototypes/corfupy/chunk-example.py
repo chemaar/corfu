33d1b61a9ad39c393ada269448955a0b578e436f
Jose María Alvarez <jmalvarez@seerc.org>
2013-03-28 16:19:35 +0200

    Example chunking

---
+++ prototypes/corfupy/chunk-example.py
@@ -0,0 +1,18 @@
+from nltk import pos_tag, word_tokenize
+import nltk
+#Read file
+sentences = ["Oracle",  "Oracle corporation"]
+#Tokenize
+token_names= [word_tokenize(sent) for sent in sentences]
+#Tag
+tagged_names = [pos_tag(item)  for item in token_names]
+#Chunk
+grammar = r"""
+NP: {<DT|PP\$>?<JJ>*<NN>} # chunk determiner/possessive,
+# adjectives and nouns
+{<NNP>+} # chunk sequences of proper nouns
+"""
+cp = nltk.RegexpParser(grammar)
+for name in tagged_names:
+    print cp.parse(name)
+    #cp.parse(tagged_tokens[1]).draw()
